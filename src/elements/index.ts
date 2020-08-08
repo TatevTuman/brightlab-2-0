ccc17ecf239cdba675858b4b57f35072a843224f
vladrose <vladdi.rozzi@gmail.com>
2020-08-08 16:32:54 +0300

    Fix Image element export

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from './Button/Button'
-export { default as Image } from './Image/Image'
 export { default as Link } from './Link/Link'
 export { default as Page } from './Page/Page'
