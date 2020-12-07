4b1bb7da108beaf492876233dcdd3c6de7e56cbc
vladrose <vladdi.rozzi@gmail.com>
2020-12-07 14:16:41 +0300

    Refactors useDelayedState logic

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
 export { default as useWindowSize } from './useWindowSize/useWindowSize'
 export { default as useWindowScroll } from './useWindowScroll/useWindowScroll'
-export { default as useDelayEffect } from './useDelayEffect/useDelayEffect'
+export { default as useDelayedState } from './useDelayedState/useDelayedState'
 export { default as useSiteMetadata } from './useSiteMetadata/useSiteMetadata'
 
 export { default as useModal } from './useModal/useModal'
